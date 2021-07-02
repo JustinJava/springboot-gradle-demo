4c5baaeba00d4f1132bc757e2c9ab0f0544c43e4
justinqin <justin_qin@foxmail.com>
2021-07-02 12:59:47 +0800

    code0702-2

--- app_common/src/main/java/com/justin/app/common/vo/DemoInfo.java
+++
@@ -1,5 +0,0 @@
-package com.justin.app.common.vo;
-
-public class DemoInfo {
-
-}
--- app_common/src/test/java/com/justin/MainTest.java
+++
@@ -1,7 +0,0 @@
-package com.justin;
-
-public class MainTest {
-    public static void main(String[] args) {
-        System.out.println("Main test finished==================");
-    }
-}
